9bba1f8281e4a651052e231f818ef191820a11f1
Amirov Foma <amirov.foma@icloud.com>
2024-06-14 11:00:12 +0300

    Initial Commit

---
+++ FlightApp/FlightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAppApp.swift
+//  FlightApp
+//
+//  Created by Amirov Foma on 14.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
